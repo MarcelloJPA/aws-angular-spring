d3c97e00b750a249796902e073cae0fd03944132
Marcello <cellolim4jpa@gmail.com>
2025-03-02 15:47:38 -0300

    Getters e Setters, configurando Swagger UI

--- src/main/java/br/com/apiproduto/entity/Produto.java
+++ src/main/java/br/com/apiproduto/entity/Produto.java
@@ -1,23 +1,64 @@
 package br.com.apiproduto.entity;
 
-import jakarta.persistence.*;
-import  lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema; // Importação das anotações Swagger
+import lombok.Data;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
-@Data //Getters, Setters, toStrings, etc
+@Table
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
     private String nome;
 
-    @Column
     private BigDecimal valor;
 
-    @Column
     private String descricao;
+
+    public Produto(){
+    }
+
+    public Produto(Long id, String nome, BigDecimal valor, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
